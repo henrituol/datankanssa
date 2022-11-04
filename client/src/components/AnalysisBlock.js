3ec2fd1ae44414bd12c61f33a8749900057b6ce7
Henri Pitkänen <henupitkanen@gmail.com>
2022-11-04 13:39:09 +0200

    Added frame for an analysis block: Hello World!

---
+++ client/src/components/AnalysisBlock.js
@@ -0,0 +1,16 @@
+const AnalysisBlock = () => {
+
+    let analysisBlockStyle = {
+        height: 300, 
+        width: 200,
+        backgroundColor: "#E6E6E3"
+    }
+
+    return(
+		<div class="analysisBlock" style={analysisBlockStyle}>
+            <p>Hello World!</p>
+		</div>
+	)
+}
+
+export default AnalysisBlock
--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -1,9 +1,13 @@
+import AnalysisBlock from "../components/AnalysisBlock";
+
 const Home = (props) => {
     return(
         <div>
             <h2>Dashboard</h2>
             <p>Analysis blocks will show up here.</p>
+            <AnalysisBlock />
         </div>
+
     )
 }
 
