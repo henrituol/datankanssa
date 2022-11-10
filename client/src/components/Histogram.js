28da6bc55c8f4480f0aa6825ed685a6c8d68b1c4
Henri Pitkänen <henupitkanen@gmail.com>
2022-11-10 14:16:04 +0200

    Testing d3js

--- client/src/components/Histogram.js
+++ client/src/components/Histogram.js
@@ -1,14 +1,31 @@
 import * as d3 from "d3";
 
-const Histogram = (props) => {
-    console.log(props)
+function Histogram(props) {
+    console.log(props);
+
+    let i = 0;
+    const activateD3 = () => {
+        if (i === 0) {
+            d3.selectAll("#histogramText").style("color", "blue");
+            d3.select(".histogram").append("p").attr("id", "helloId").text("Hello!");
+            i = 1;
+
+        } else {
+            d3.selectAll("#histogramText").style("color", "black");
+            d3.select("#helloId").remove("p")
+            i = 0;
+        }
+    }
+
     return (
-        <div>
-            <p>
+        <div className="histogram">
+            <button onClick={activateD3}>Change view with d3.js</button>
+            <p id="histogramText">
                 {props.data}
             </p>
         </div>
-    )
+    );
+
 }
 
 export default Histogram;
