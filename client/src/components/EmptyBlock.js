de420ff5f8a13f0cb5ac9099599b35b725c0d231
Henri Pitkänen <henupitkanen@gmail.com>
2022-11-15 16:16:45 +0200

    Added console log for button clicks

--- client/src/components/EmptyBlock.js
+++ client/src/components/EmptyBlock.js
@@ -1,4 +1,9 @@
+import { useState } from "react";
+import AnalysisBlock from "./AnalysisBlock"
 
+// When a button is pushed, a new view should appear.
+// In the new view, you can select the type of visualization and data.
+// However, in this first version: click +-button and render a pie chart with dummy data.
 
 const EmptyBlock = () => {
 
@@ -9,10 +14,16 @@ const EmptyBlock = () => {
         backgroundColor: "#E6E6E3"
     }
 
+    let tempData = [45, 5, 20, 25, 5];
+
+    function chooseView() {
+        console.log("Clicked +-button")
+    }
 
     return (
         <div className="EmptyBlock" style={analysisBlockStyle}>
             <p>Press + to add an analysis view.</p>
+            <button onClick={chooseView}>+</button>
         </div>
     )
 
