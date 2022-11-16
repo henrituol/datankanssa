73f1f2d475c66e89a78c888baeefd2091bafcf1c
Henri Pitkänen <henupitkanen@gmail.com>
2022-11-16 15:20:22 +0200

    Since adding a block works now, deleted demos

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -7,16 +7,9 @@ const Dashboard = () => {
     // whereupon props value is placed on {props.visualizationType}
     // Then again, is Dashboard even the right component to give these values?
 
-    let tempPercentageData = [20, 15, 15, 45, 5];
-    // Dummy data. Could be, e.g., customer satisfaction reviews on likert scale.
-    let tempBarGraphData = [1, 1, 1, 2, 2, 3, 4, 4, 4, 4, 5, 5, 5];
-
     return(
         <div className =  "mainAnalysis">
             <h2>Dashboard</h2>
-            <p>The analysis blocks will show up here.</p>
-            <AnalysisBlock visualizationType = "piechart" data = {tempPercentageData} />
-            <AnalysisBlock visualizationType = "bargraph" data = {tempBarGraphData} />
             <EmptyBlock />
         </div>
 
