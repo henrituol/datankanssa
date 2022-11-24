17dd147347d773b1041d61525614c5761f56ff2f
Henri Pitkänen <henri.pitkanen@siili.com>
2022-11-24 09:20:40 +0200

    Removed redundant parts

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -1,11 +1,6 @@
-import AnalysisBlock from "../components/AnalysisBlock";
 import EmptyBlock from "../components/EmptyBlock";
 
 const Dashboard = () => {
-
-    // AnalysisBlock sends a value in props to AnalysisBlock.js
-    // whereupon props value is placed on {props.visualizationType}
-    // Then again, is Dashboard even the right component to give these values?
 
     return(
         <div className =  "mainAnalysis">
