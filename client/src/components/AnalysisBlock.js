be7555113fda216fe137652eb7e2a8e1bce65764
Henri Pitkänen <henri.pitkanen@siili.com>
2022-11-24 09:35:02 +0200

    Refactored if-statements to a more readable swith case

--- client/src/components/AnalysisBlock.js
+++ client/src/components/AnalysisBlock.js
@@ -1,10 +1,6 @@
 import BarGraph from "./BarGraph";
 import PieChart from "./PieChart";
 
-// This component is called in Dashboard.
-// From dashboard we get props: visualizationType and data.
-// props.data will be further send to Histogram.js
-
 const AnalysisBlock = (props) => {
 
     let analysisBlockStyle = {
@@ -15,26 +11,26 @@ const AnalysisBlock = (props) => {
     }
 
     // Conditional rendering depending on the type of visualization.
-    if (props.visualizationType === "bargraph") {
-        return (
-            <div className="analysisBlock" style={analysisBlockStyle}>
-                <BarGraph data = {props.data} name = {"Demonstration bar graph"} />
-            </div>
-        )
-    } else if (props.visualizationType === "piechart") {
-        return (
-            <div className="analysisBlock" style={analysisBlockStyle}>
-            <PieChart data = {props.data} name = {"Demonstration pie chart"} />
-            </div>
-        )
-    } else {
-        return (
-            <div className="analysisBlock" style={analysisBlockStyle}>
-                <p>Did you specify a proper type for visualization?</p>
-            </div>
-        )
+    switch (props.visualizationType) {
+        case "bargraph":
+            return (
+                <div className="analysisBlock" style={analysisBlockStyle}>
+                    <BarGraph data = {props.data} name = {"Demonstration bar graph"} />
+                </div>
+            )
+        case "piechart":
+            return (
+                <div className="analysisBlock" style={analysisBlockStyle}>
+                <PieChart data = {props.data} name = {"Demonstration pie chart"} />
+                </div>
+            )
+        default:
+            return (
+                <div className="analysisBlock" style={analysisBlockStyle}>
+                    <p>Did you specify a proper type for visualization?</p>
+                </div>
+            )
     }
-
 }
 
 export default AnalysisBlock
