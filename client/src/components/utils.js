0017fb1a809f2d5c9215ffb6e3ccd2185cc763fc
Henri Pitkänen <henri.pitkanen@siili.com>
2022-11-24 10:18:23 +0200

    Added a helper for changing background style of a block

--- client/src/components/utils.js
+++ client/src/components/utils.js
@@ -2,7 +2,7 @@
 
 // Create a unique ID for a div, so that d3js visualization can be matched with the proper container.
 export function uniqueIdCreator () {
-    
+
     let divId = Math.floor(Math.random() * 99999);
     // Let's make sure the ID is unique.
     if (document.getElementById(divId) !== null) {
@@ -13,4 +13,16 @@ export function uniqueIdCreator () {
     let uniqueDiv = "Id" + divId;
 
     return uniqueDiv;
+}
+
+
+export function blockBackgroundStyle () {
+    let analysisBlockStyle = {
+        height: 300, 
+        width: 300,
+        margin: 5,
+        backgroundColor: "#E6E6E3"
+    }
+
+    return analysisBlockStyle;
 }
