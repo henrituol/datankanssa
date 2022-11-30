548e3a2ff77b4be9e63e17349e22fc4ed960f408
Henri Pitkänen <henri.pitkanen@siili.com>
2022-11-30 15:28:31 +0200

    Added dates to query

--- client/src/components/FetchData.js
+++ client/src/components/FetchData.js
@@ -19,14 +19,32 @@ const FetchData = (props) => {
     // At the time of writing, 1.0 is public, but with 1.1 it is possible to query only selected parameters.
     // Also, specify for the fetch that we want text/csv type data.
 
-    // Also, render the fetched data.
-    // I.e. "render as you fetch":
-    // There use to React Suspense, but it is outdated apparently.
+    // And before we continue, the query is modified according to selected dates.
+    // props.dates is an array with two dates in the form yyyy-mm-dd.
+    // First one is starting date and second one is ending date.
+    // Split years, months, dates etc. to their own variables.
+
+
+    const startMilliseconds = new Date(props.dates[0]).getTime();
+    console.log(startMilliseconds);
+    const endMilliseconds = new Date(props.dates[1]).getTime();
+    console.log(endMilliseconds);
 
     async function loadData () {
-        const offset = Math.floor(Math.random() * 5);
 
-        const target = "https://hsl.louhin.com/api/1.1/data/350/content?offset=" + offset + "0000&limit=1000&variables=K3B&LWSAccessKey=b21f0e72-de32-4cee-ab24-242eeba7726b";
+        // Here's the place to edit the query.
+        // Basic testing query:
+        // const target = "https://hsl.louhin.com/api/1.1/data/350/content?limit=1000&variables=K3B&LWSAccessKey=b21f0e72-de32-4cee-ab24-242eeba7726b";
+
+        // Seems like the PÄIVÄMÄÄRÄ doesn't work in the api version 1.1...
+        const target = "https://hsl.louhin.com/api/1.1/data/350/content?limit=1000&variables=K3B&filter[PÄIVÄMÄÄRÄ]=" + startMilliseconds + "to" + endMilliseconds + "&LWSAccessKey=b21f0e72-de32-4cee-ab24-242eeba7726b";
+        // It does give 200 message in Insomnia, but no data...
+
+        // Antti Vuorela said everyhting should work as it used to, but that's not how it looks like.
+        // In insomnia:
+        // https://hsl.louhin.com/api/1.1/data/350/content?limit=100&filter[PÄIVÄMÄÄRÄ]=1356991200000to1388527199000 this doesn't load data. Without the filter it works fine.
+        // https://hsl.louhin.com/api/1.0/data/350?limit=100&filter[PÄIVÄMÄÄRÄ]=1356991200000to1388527199000 this does.
+
         return await fetch( target, {
             method: 'get',
             headers: {
