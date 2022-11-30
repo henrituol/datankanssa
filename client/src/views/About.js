8f66e2bf711b8ab1f8823a403d94c9d0a41903e9
Henri Pitkänen <henri.pitkanen@siili.com>
2022-11-30 09:55:20 +0200

    Added link to a site that explains what is MERN stack

--- client/src/views/About.js
+++ client/src/views/About.js
@@ -3,7 +3,7 @@ const About = () => {
     return(
         <div>
             <h2>About</h2>
-            <p>"Datankanssa – A Business Intelligence Suite" is a MERN stack exercise by Henri Pitkänen</p>
+            <p>"Datankanssa – A Business Intelligence Suite" is a <a href= "https://www.mongodb.com/mern-stack" target="_blank">MERN stack</a> exercise by Henri Pitkänen.</p>
         </div>
     )
 }
