b0256ceaca793400f6bfc9fc1e9f95b977c63af5
Henri Pitkänen <henri.pitkanen@siili.com>
2022-12-01 09:58:03 +0200

    Fixed Asty query with date

--- client/src/components/FetchData.js
+++ client/src/components/FetchData.js
@@ -3,7 +3,7 @@ import LoadingBlock from "./LoadingBlock.js"
 import AnalysisBlock from "./AnalysisBlock"
 import EmptyBlock from "./EmptyBlock.js";
 
-// Next, create a new analysis block and another empty block.
+// Create a new analysis block and another empty block.
 // To have meaningful visualizations, let's utilize HSL Asty customer satisfaction survey:
 // https://hsl.louhin.com/asty/
 
@@ -17,45 +17,32 @@ const FetchData = (props) => {
     // Query Asty Data API
     // https://hsl.louhin.com/asty/help
     // At the time of writing, 1.0 is public, but with 1.1 it is possible to query only selected parameters.
-    // Also, specify for the fetch that we want text/csv type data.
+    // - Querying all the data would take a lot of time, because dataset is rather big.
 
     // And before we continue, the query is modified according to selected dates.
     // props.dates is an array with two dates in the form yyyy-mm-dd.
     // First one is starting date and second one is ending date.
-    // Split years, months, dates etc. to their own variables.
+    // Although API version 1.0 instructs to use milliseconds,
+    // I noticed that with the API version 1.1 milliseconds do not work, but format 2011-01-01 is usable.
+    // Hence, we can use the dates as is in the props.dates.
 
-
-    const startMilliseconds = new Date(props.dates[0]).getTime();
-    console.log(startMilliseconds);
-    const endMilliseconds = new Date(props.dates[1]).getTime();
-    console.log(endMilliseconds);
 
     async function loadData () {
 
-        // Here's the place to edit the query.
-        // Basic testing query:
-        // const target = "https://hsl.louhin.com/api/1.1/data/350/content?limit=1000&variables=K3B&LWSAccessKey=b21f0e72-de32-4cee-ab24-242eeba7726b";
+        // Here's the place to edit the query, if needed
+        const startDate = props.dates[0];
+        const endDate = props.dates[1];
 
-        // Seems like the PÄIVÄMÄÄRÄ doesn't work in the api version 1.1...
-        const target = "https://hsl.louhin.com/api/1.1/data/350/content?limit=1000&variables=K3B&filter[PÄIVÄMÄÄRÄ]=" + startMilliseconds + "to" + endMilliseconds + "&LWSAccessKey=b21f0e72-de32-4cee-ab24-242eeba7726b";
-        // It does give 200 message in Insomnia, but no data...
+        const target = `https://hsl.louhin.com/api/1.1/data/350/content?limit=1000&variables=K3B&filter[PÄIVÄMÄÄRÄ]=` + startDate + "to" + endDate + `&LWSAccessKey=b21f0e72-de32-4cee-ab24-242eeba7726b`;
 
-        // Everything should work as it used to, but that's not how it looks like.
-        // In insomnia:
-        // https://hsl.louhin.com/api/1.1/data/350/content?limit=100&filter[PÄIVÄMÄÄRÄ]=1356991200000to1388527199000 this doesn't load data. Without the filter it works fine.
-        // https://hsl.louhin.com/api/1.0/data/350?limit=100&filter[PÄIVÄMÄÄRÄ]=1356991200000to1388527199000 this does.
-
-        // A possible workaround might be something like this:
-        // Fetch data as a whole containing only the variable we're visualizing and dates, and afterwards filter by dates.
-        // How long does the query take? 
-
+        // Specify that the data which we are fetching is in text/csv type.
         return await fetch( target, {
             method: 'get',
             headers: {
                 'content-type': 'text/csv;charset=UTF-8',
             }
         })
-        .then(response => response.text()) // There has to be clearer solution to this. See, bunch of RegEx down.
+        .then(response => response.text()) // There has to be clearer solution to this. See, bunch of RegEx down...
         .then((response) => {
             return response;
         })
