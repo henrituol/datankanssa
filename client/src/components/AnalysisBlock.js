baa0c1fd0cb50492a14d01e8521e755a304a0606
Henri Pitkänen <henri.pitkanen@siili.com>
2022-12-01 11:19:44 +0200

    Updated block title to more descriptive version

--- client/src/components/AnalysisBlock.js
+++ client/src/components/AnalysisBlock.js
@@ -9,17 +9,18 @@ const AnalysisBlock = (props) => {
     let analysisBlockStyle = blockBackgroundStyle ();
 
     // Conditional rendering depending on the type of visualization.
+    // These props come from FetchData.js.
     switch (props.visualizationType) {
         case "bargraph":
             return (
                 <div className="analysisBlock" style={analysisBlockStyle}>
-                    <BarGraph data = {props.data} name = {"Demonstration bar graph"} />
+                    <BarGraph data = {props.data} name = {props.name} />
                 </div>
             )
         case "piechart":
             return (
                 <div className="analysisBlock" style={analysisBlockStyle}>
-                <PieChart data = {props.data} name = {"Demonstration pie chart"} />
+                <PieChart data = {props.data} name = {props.name} />
                 </div>
             )
         default:
